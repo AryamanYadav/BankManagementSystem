58ed8d550a67c300d48d89597bb6f90edef5247f
Aryaman Yadav <asyadav462@gmail.com>
2025-11-10 12:34:44 +0530

    commiting on 10nov

--- src/main/java/com/Aryaman/BankManagementSystem/Repository/UserRepo.java
+++ src/main/java/com/Aryaman/BankManagementSystem/Repository/UserRepo.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepo extends JpaRepository<User, Integer> {
+public interface UserRepo extends JpaRepository<User,Integer> {
 
-    Optional<User> findByUserName(String username);
+    Optional<User> findByUserName(String userName);
+
+
 }
